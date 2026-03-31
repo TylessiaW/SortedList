0fde3b3523b0bc6d3c1f0a833cebe3c36a1a8833
Tylessia W <TylessiaWillis@gmail.com>
2026-03-31 17:43:20 -0500

    Fixed errors in SortedList

--- SortedList/src/SortedList.java
+++ SortedList/src/SortedList.java
@@ -1,106 +1,110 @@
 public class SortedList<T extends Comparable<T>> {
 
-	private class Node {
-		T data;
-		Node next;
+    private class Node {
+        T data;
+        Node next;
 
-		Node(T data) {
-			this.data = data;
-			this.next = null;
-		}
-	}
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-	private Node head;
-	private int size;
+    private Node head;
+    private int size;
 
-	public SortedList() {
-		this.head = null;
-		this.size = 0;
-	}
+    public SortedList() {
+        head = null;
+        size = 0;
+    }
 
-	public void add(T element) {
-		Node newNode = new Node(element);
+    // Add element in sorted order
+    public void add(T element) {
+        Node newNode = new Node(element);
 
-		if (head == null || element.compareTo(head.data) < 0) {
-			newNode.next = head;
-			head = newNode;
-		} else {
-			Node current = head;
+        // Case 1: empty list OR insert at beginning
+        if (head == null || element.compareTo(head.data) < 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
 
-			while (current.next !=null &&
-				element.compareTo(current.next.data) > 0) {
-					current = current.next;
-				}
+            // find correct position
+            while (current.next != null &&
+                   element.compareTo(current.next.data) > 0) {
+                current = current.next;
+            }
 
-			newNode.next = current.next;
-			current.next = newNode;
-		}
+            newNode.next = current.next;
+            current.next = newNode;
+        }
 
-		size++;
-	}
+        size++;
+    }
 
-	public boolean remove(T element) {
-		if (head == null) {
-			return false;
-		}
+    // Remove element
+    public boolean remove(T element) {
+        if (head == null) {
+            return false;
+        }
 
-		if (head.data.equals(element)) {
-			head = head.next;
-			size--;
-			return true;
-		}
+        // removing head
+        if (head.data.equals(element)) {
+            head = head.next;
+            size--;
+            return true;
+        }
 
-		Node current = head;
+        Node current = head;
 
-		while (current.next != null &&
-			!current.next.data.equals(element)) {
-				current = current.next;
-	}
+        while (current.next != null &&
+               !current.next.data.equals(element)) {
+            current = current.next;
+        }
 
-	if (current.next == null) {
-		return false;
-	}
+        if (current.next == null) {
+            return false;
+        }
 
-	current.next = current.next.next;
-	size--;
-	return true;
-	}
+        current.next = current.next.next;
+        size--;
+        return true;
+    }
 
-	public T get(int index) {
-		if (index <0 || index >= size) {
-			throw new IndexOutBoundsException();
-		}
+    // Get element at index
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
 
-		Node current = head;
+        Node current = head;
 
-		for (int i = 0;  i < index; i++) {
-			current = current.next;
-		}
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
 
-		return current.data;
-	}
+        return current.data;
+    }
 
-	public int size() {
-		return size;
-	}
+    // Size of list
+    public int size() {
+        return size;
+    }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    // Check if empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    // Print list (useful for testing)
+    public void printList() {
+        Node current = head;
 
-	public void printlist() {
-		Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
 
-		while(current != null) {
-			System.out.print(current.data + " ->");
-			current = current.next;
-		}
-
-		System.out.println("null");
-	}
-
+        System.out.println("null");
+    }
 }
